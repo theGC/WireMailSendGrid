15ac3e0023b321fbdf41b2e670f5b2edebdec0ba
Guy Campbell <guy@thebigsurf.co.uk>
2020-01-12 12:30:06 +0000

    update to v7.4.0 of sendgrid-php

--- sendgrid-php/vendor/composer/autoload_psr4.php
+++ sendgrid-php/vendor/composer/autoload_psr4.php
@@ -6,8 +6,16 @@ $vendorDir = dirname(dirname(__FILE__));
 $baseDir = dirname($vendorDir);
 
 return array(
+    'phpDocumentor\\Reflection\\' => array($vendorDir . '/phpdocumentor/reflection-common/src', $vendorDir . '/phpdocumentor/reflection-docblock/src', $vendorDir . '/phpdocumentor/type-resolver/src'),
+    'Webmozart\\Assert\\' => array($vendorDir . '/webmozart/assert/src'),
+    'Symfony\\Polyfill\\Ctype\\' => array($vendorDir . '/symfony/polyfill-ctype'),
+    'Swaggest\\JsonDiff\\' => array($vendorDir . '/swaggest/json-diff/src'),
+    'SendGrid\\Tests\\' => array($baseDir . '/test'),
     'SendGrid\\Stats\\' => array($baseDir . '/lib/stats'),
     'SendGrid\\Mail\\' => array($baseDir . '/lib/mail'),
     'SendGrid\\Contacts\\' => array($baseDir . '/lib/contacts'),
     'SendGrid\\' => array($baseDir . '/lib', $vendorDir . '/sendgrid/php-http-client/lib'),
+    'Prophecy\\' => array($vendorDir . '/phpspec/prophecy/src/Prophecy'),
+    'Doctrine\\Instantiator\\' => array($vendorDir . '/doctrine/instantiator/src/Doctrine/Instantiator'),
+    'DeepCopy\\' => array($vendorDir . '/myclabs/deep-copy/src/DeepCopy'),
 );
